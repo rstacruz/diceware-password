30628ae46938ee45ed8c3fa1514577fff3c472bd
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-08-06 12:25:39 +1000

    Add initial implementation

---
+++ src/_clap.rs
@@ -0,0 +1,13 @@
+// extern crate clap;
+// use clap::{App, Arg};
+// let app = App::new("Passgen").version("0.0.1").arg(
+//     Arg::with_name("words")
+//         .short("w")
+//         .long("words")
+//         .help("Number of words")
+//         .value_name("N")
+//         .takes_value(true),
+// );
+
+// let matches = app.get_matches();
+// println!("{:?}", matches);
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,88 @@
+extern crate rand;
+use rand::Rng;
+
+const WORDS_SIZE: usize = 60;
+
+const WORDS: [&str; WORDS_SIZE] = [
+    "common",
+    "book",
+    "electronic",
+    "talk",
+    "account",
+    "mark",
+    "interest",
+    "written",
+    "can't",
+    "bed",
+    "necessary",
+    "age",
+    "else",
+    "force",
+    "idea",
+    "longer",
+    "art",
+    "spoke",
+    "across",
+    "brother",
+    "early",
+    "ought",
+    "sometimes",
+    "line",
+    "saying",
+    "table",
+    "appeared",
+    "river",
+    "continued",
+    "eye",
+    "ety",
+    "sun",
+    "information",
+    "later",
+    "everything",
+    "reached",
+    "suddenly",
+    "past",
+    "hours",
+    "strange",
+    "deep",
+    "change",
+    "miles",
+    "feeling",
+    "act",
+    "meet",
+    "paid",
+    "further",
+    "purpose",
+    "happy",
+    "added",
+    "seem",
+    "taking",
+    "blood",
+    "rose",
+    "south",
+    "beyond",
+    "cold",
+    "neither",
+    "forward",
+];
+
 fn main() {
-    println!("Hello, world!");
+    let rng = rand::thread_rng();
+
+    let passwords: Vec<&str> = vec![
+        get_random_word(rng),
+        get_random_word(rng),
+        get_random_word(rng),
+        get_random_word(rng),
+    ];
+
+    println!(
+        "{} {} {} {}",
+        passwords[0], passwords[1], passwords[2], passwords[3]
+    );
+}
+
+fn get_random_word<'a>(mut rng: rand::prelude::ThreadRng) -> &'a str {
+    let n: usize = rng.gen_range(0, WORDS_SIZE);
+    &WORDS[n]
 }
