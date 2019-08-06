a8e1b37a33ff77d7bd689e5aafcd355a1c5dfde0
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-08-06 14:33:32 +1000

    Refactor generator

---
+++ src/generator.rs
@@ -0,0 +1,73 @@
+extern crate rand;
+use crate::words::{WORDS, WORDS_SIZE};
+use rand::prelude::ThreadRng;
+use rand::Rng;
+
+pub struct Generator {
+    /// Random number generator
+    rng: ThreadRng,
+
+    /// Number of words to generate
+    word_count: u64,
+
+    /// If spaces are to be used
+    spaces: bool,
+}
+
+impl Generator {
+    /// Create a new Generator
+    pub fn new() -> Generator {
+        Generator {
+            rng: rand::thread_rng(),
+            word_count: 4,
+            spaces: true,
+        }
+    }
+
+    pub fn word_count(mut self, n: u64) -> Generator {
+        self.word_count = n;
+        self
+    }
+
+    pub fn spaces(mut self, value: bool) -> Generator {
+        self.spaces = value;
+        self
+    }
+
+    /// Generate a password
+    ///
+    /// ```
+    /// gen = Generator::new();
+    /// gen.generate()
+    /// // => "correct horse battery staple"
+    /// ```
+    pub fn generate(&self) -> String {
+        let count = self.word_count;
+        let words = self.get_words(count);
+        if self.spaces {
+            words.join(" ")
+        } else {
+            words.join("-")
+        }
+    }
+
+    /// Returns `count` random words
+    pub fn get_words<'a>(&self, count: u64) -> Vec<&'a str> {
+        let mut words: Vec<&str> = vec![];
+        let mut index = 0;
+
+        while index < count {
+            words.push(self.get_random_word());
+            index += 1;
+        }
+
+        words
+    }
+
+    /// Returns one random word
+    pub fn get_random_word<'a>(&self) -> &'a str {
+        let mut rng = self.rng;
+        let n: usize = rng.gen_range(0, WORDS_SIZE);
+        &WORDS[n]
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,10 @@
+mod generator;
 mod words;
 
-extern crate rand;
-use rand::prelude::ThreadRng;
-use rand::Rng;
-use words::{WORDS, WORDS_SIZE};
+use generator::Generator;
 
 fn main() {
-    let gen = Generator::new();
-    let passwd = gen.generate();
-    println!("{}", passwd);
-}
+    let gen = Generator::new().word_count(4).spaces(true);
 
-struct Generator {
-    rng: ThreadRng,
-}
-
-impl Generator {
-    pub fn new() -> Generator {
-        Generator {
-            rng: rand::thread_rng(),
-        }
-    }
-
-    pub fn generate(&self) -> String {
-        let words = self.get_words();
-        let str = format!("{} {} {} {}", words[0], words[1], words[2], words[3]);
-        str
-    }
-
-    pub fn get_words<'a>(&self) -> Vec<&'a str> {
-        vec![
-            self.get_random_word(),
-            self.get_random_word(),
-            self.get_random_word(),
-            self.get_random_word(),
-        ]
-    }
-
-    pub fn get_random_word<'a>(&self) -> &'a str {
-        let mut rng = self.rng;
-        let n: usize = rng.gen_range(0, WORDS_SIZE);
-        &WORDS[n]
-    }
+    println!("{}", gen.generate());
 }
