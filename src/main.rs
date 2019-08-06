0de3bf384d2d42e580e3c52b6b8e8906e7e9e225
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-08-06 12:30:19 +1000

    Refactor words to be in a separate module

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,27 @@
+mod words;
+
 extern crate rand;
+use rand::prelude::ThreadRng;
 use rand::Rng;
-
-const WORDS_SIZE: usize = 60;
-
-const WORDS: [&str; WORDS_SIZE] = [
-    "common",
-    "book",
-    "electronic",
-    "talk",
-    "account",
-    "mark",
-    "interest",
-    "written",
-    "can't",
-    "bed",
-    "necessary",
-    "age",
-    "else",
-    "force",
-    "idea",
-    "longer",
-    "art",
-    "spoke",
-    "across",
-    "brother",
-    "early",
-    "ought",
-    "sometimes",
-    "line",
-    "saying",
-    "table",
-    "appeared",
-    "river",
-    "continued",
-    "eye",
-    "ety",
-    "sun",
-    "information",
-    "later",
-    "everything",
-    "reached",
-    "suddenly",
-    "past",
-    "hours",
-    "strange",
-    "deep",
-    "change",
-    "miles",
-    "feeling",
-    "act",
-    "meet",
-    "paid",
-    "further",
-    "purpose",
-    "happy",
-    "added",
-    "seem",
-    "taking",
-    "blood",
-    "rose",
-    "south",
-    "beyond",
-    "cold",
-    "neither",
-    "forward",
-];
+use words::{WORDS, WORDS_SIZE};
 
 fn main() {
     let rng = rand::thread_rng();
+    let words = get_words(rng);
 
-    let passwords: Vec<&str> = vec![
-        get_random_word(rng),
-        get_random_word(rng),
-        get_random_word(rng),
-        get_random_word(rng),
-    ];
-
-    println!(
-        "{} {} {} {}",
-        passwords[0], passwords[1], passwords[2], passwords[3]
-    );
+    println!("{} {} {} {}", words[0], words[1], words[2], words[3]);
 }
 
-fn get_random_word<'a>(mut rng: rand::prelude::ThreadRng) -> &'a str {
+fn get_words<'a>(rng: ThreadRng) -> Vec<&'a str> {
+    vec![
+        get_random_word(rng),
+        get_random_word(rng),
+        get_random_word(rng),
+        get_random_word(rng),
+    ]
+}
+
+fn get_random_word<'a>(mut rng: ThreadRng) -> &'a str {
     let n: usize = rng.gen_range(0, WORDS_SIZE);
     &WORDS[n]
 }
