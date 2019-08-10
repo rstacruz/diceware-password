3cfc537fe50ee6a9e1ea028db0280d8b963af756
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-08-10 23:11:40 +1000

    Test passwords

--- src/generator/tests.rs
+++ src/generator/tests.rs
@@ -1,4 +1,6 @@
+extern crate regex;
 use super::*;
+use regex::Regex;
 
 #[test]
 fn it_has_defaults() {
@@ -15,9 +17,16 @@ fn it_can_set_things() {
 }
 
 #[test]
-fn it_can_generate() {
-    for _x in 0..10 {
-        let gen = Generator::new().generate();
-        assert!(gen.len() > 0);
+fn it_can_generate_passwords() {
+    let has_spaces = Regex::new(r" ").unwrap();
+    let has_letters = Regex::new(r"[a-zA-Z]").unwrap();
+    let has_numbers = Regex::new(r"[0-9]").unwrap();
+
+    for _x in 0..100 {
+        let result = Generator::new().generate();
+        assert!(result.len() > 0);
+        assert!(has_spaces.is_match(&result));
+        assert!(has_letters.is_match(&result));
+        assert!(has_numbers.is_match(&result));
     }
 }
